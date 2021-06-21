dc05c23ae50aba6a50b50dc44fd3d8a6bfb48e2a
John Palenchar <jpalenchar.3@gmail.com>
2021-06-21 13:54:43 -0400

    make diff

--- Tester.js
+++ Tester.js
@@ -1,1 +1,3 @@
 const FS = require('fs');
+let data = JSON.parse(FS.readFileSync('./sample.dtf'));
+let initial = JSON.parse(FS.readFileSync('./initial.dtf'));
--- Tester.ts
+++ Tester.ts
@@ -1,2 +1,5 @@
 const FS = require('fs');
 
+let data = JSON.parse(FS.readFileSync('./sample.dtf'));
+let initial = JSON.parse(FS.readFileSync('./initial.dtf'));
+
