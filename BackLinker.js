e435bee7246024d93609828d2d8432e8aaed39f9
John Palenchar <jpalenchar.3@gmail.com>
2021-06-21 14:58:06 -0400

    stuff

--- BackLinker.js
+++ BackLinker.js
@@ -2,3 +2,4 @@ const FS = require('fs');
 const difference = require('difference');
 let data = JSON.parse(FS.readFileSync('./sample.dtf'));
 let initial = JSON.parse(FS.readFileSync('./initial.dtf'));
+(difference.findDiff("hello"));
--- BackLinker.ts
+++ BackLinker.ts
@@ -1,7 +1,16 @@
 const FS = require('fs');
-
 const difference = require('difference');
+
+interface Changes {
+  esn: string,
+  module: number,
+  cell: number,
+  pin: number,
+  keys: string[],
+  values: number[]
+}
 
 let data = JSON.parse(FS.readFileSync('./sample.dtf'));
 let initial = JSON.parse(FS.readFileSync('./initial.dtf'));
 
+(difference.findDiff("hello"));
