af2ba803bf2253d104ecb19aee29f9ecad4affeb
John Palenchar <jpalenchar.3@gmail.com>
2021-06-24 08:49:16 -0400

    update gitignore

--- BackLinker.js
+++ BackLinker.js
@@ -5,7 +5,10 @@ let data = JSON.parse(FS.readFileSync("./sample.dtf", "utf-8"));
 let initial = JSON.parse(FS.readFileSync('./initial.dtf', "utf-8"));
 let diff = DF.DifferenceFinder(initial, data);
 let board = emptyBoard(diff[0].esn, initial.time);
-console.log(board);
 for (let i = 0; i < diff.length; i++) {
-    const element = diff[i];
+    for (let j = 0; j < diff[i].keys.length; j++) {
+        let key = diff[i].keys[j];
+        board.modules[diff[i].module].cells[diff[i].cell].pins[diff[i].pin][key] = diff[i].values[j];
+    }
 }
+console.log(`done`);
--- BackLinker.ts
+++ BackLinker.ts
@@ -1,18 +1,19 @@
 import * as FS from 'fs';
 import * as DF from './difference/difference.js';
-import {JSONSaver,emptyBoard} from './supportingJs/supporting.js';
+import { JSONSaver, emptyBoard } from './supportingJs/supporting.js';
 
-let data = JSON.parse(FS.readFileSync("./sample.dtf","utf-8"));
-let initial = JSON.parse(FS.readFileSync('./initial.dtf',"utf-8"));
+let data = JSON.parse(FS.readFileSync("./sample.dtf", "utf-8"));
+let initial = JSON.parse(FS.readFileSync('./initial.dtf', "utf-8"));
 
 let diff = DF.DifferenceFinder(initial, data);
 
-let board = emptyBoard(diff[0].esn,initial.time);
-
-console.log(board);
-
+let board = emptyBoard(diff[0].esn, initial.time);
 
 for (let i = 0; i < diff.length; i++) {
-  const element = diff[i];
+  for (let j = 0; j < diff[i].keys.length; j++) {
+    let key = diff[i].keys[j];
+    board.modules[diff[i].module].cells[diff[i].cell].pins[diff[i].pin][key] = diff[i].values[j]
+  }
 }
 
+console.log(`done`);
