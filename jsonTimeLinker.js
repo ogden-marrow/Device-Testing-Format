a91861f37410f867ad0fad198b2559381f23664b
John Palenchar <jpalenchar.3@gmail.com>
2021-06-24 09:11:19 -0400

    more .gitignore

--- jsonTimeLinker.js
+++
@@ -1,8 +0,0 @@
-/*jshint esversion: 6 */
-//when one tries to modify the json the Reference the previous version
-const fs = require('fs');
-const Diff = require('difference');
-// Read in and parse the json
-let newData = JSON.parse(fs.readFileSync('./sample.dtf'));
-let OGData = JSON.parse(fs.readFileSync('./initial.dtf'));
-console.log(Diff.findDiff(OGData, newData));
