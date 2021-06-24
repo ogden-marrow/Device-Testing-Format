e2ba185ac67341bb9a8e259839d43410f56a0464
John Palenchar <jpalenchar.3@gmail.com>
2021-06-24 14:12:46 -0400

    getdata testing and updates

--- dataFetcher/GetData.ts
+++ dataFetcher/GetData.ts
@@ -1,5 +1,16 @@
-import { dtfParse, JSONSaver } from '../supportingJs/supporting.js';
+import { JSONSaver } from '../supportingJs/supporting.js';
+import { board } from '../dtfDescription/dtfDescription.js';
 
-function QueryParser (params:type) {
-  
+function QueryFromESNTime(esn: string, Time: number, Module?: number, Cell?: number, Pin?: number,) {
+  if ((typeof Module === 'undefined') && ((typeof Cell != 'undefined') || (typeof Pin != 'undefined'))) {
+    throw new Error('To select a specific pin or cell a module is required')
+  } else if ((typeof Cell === 'undefined') && (typeof Pin != 'undefined')){
+    throw new Error('To select a specific pin a module and a cell are required')
+  }
+  console.log('yep that worked');
 }
+function QueryFromBoardObject(Board: board, Module?: number, Cell?: Number, Pin?: number) {
+
+}
+
+export { QueryFromBoardObject, QueryFromESNTime }
