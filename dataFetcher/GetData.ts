141981956e6e845f71622b42ba131b3b2788f1c6
John Palenchar <jpalenchar.3@gmail.com>
2021-06-25 14:59:05 -0400

    Working on the backLinkSearch functions

--- dataFetcher/GetData.ts
+++ dataFetcher/GetData.ts
@@ -1,15 +1,60 @@
-import { JSONSaver } from '../supportingJs/supporting.js';
-import { board } from '../dtfDescription/dtfDescription.js';
+import { findFromESN, JSONSaver, findLatestOf} from '../supportingJs/supporting.js';
+import { board, module, cell, pin } from '../dtfDescription/dtfDescription.js';
 
-function QueryFromESNTime(esn: string, Time: number, Module?: number, Cell?: number, Pin?: number,) {
-  if ((typeof Module === 'undefined') && ((typeof Cell != 'undefined') || (typeof Pin != 'undefined'))) {
+function QueryFromESNTime(esn: string, Time?: number, Module?: number, Cell?: number, Pin?: number,): board | module | cell | pin {
+  if ((typeof (Module) == 'undefined') && ((typeof (Cell) != 'undefined') || (typeof (Pin) != 'undefined'))) {
     throw new Error('To select a specific pin or cell a module is required')
-  } else if ((typeof Cell === 'undefined') && (typeof Pin != 'undefined')){
+  } else if ((typeof (Cell) == 'undefined') && (typeof (Pin) != 'undefined')) {
+    throw new Error('To select a specific pin a module and a cell are required')
+  } else if (typeof (Module) == 'undefined') {
+    if (typeof (Time) != 'undefined') {
+      return findFromESN(esn, Time)
+    } else {
+      return findFromESN(esn)
+    }
+  } else if (typeof (Module) == 'number' && (typeof (Cell) == 'undefined')) {
+    if (typeof (Time) != 'undefined') {
+      return QueryForModule(findFromESN(esn, Time), Module);
+    } else {
+      return QueryForModule(findFromESN(esn), Module);
+    }
+  } else if (typeof (Cell) == 'number' && (typeof (Pin) == 'undefined')) {
+    if (typeof (Time) != 'undefined') {
+      return QueryForCell(findFromESN(esn, Time), Module, Cell)
+    } else {
+      return QueryForCell(findFromESN(esn), Module, Cell);
+    }
+  } else if (typeof (Pin) == 'number') {
+    if (typeof (Time) != 'undefined') {
+      return QueryForPin(findFromESN(esn, Time), Module, Cell, Pin);
+    } else {
+      return QueryForPin(findFromESN(esn), Module, Cell, Pin);
+    }
+  }
+}
+
+function QueryForModule(Board: board, ModNumber: number){
+  if (Object.keys(Board.modules[ModNumber]).length == 0) {
+    return findLatestOf(Board,ModNumber);
+  } else {
+    return Board.modules[ModNumber]
+  }
+}
+function QueryForCell(Board: board, ModNumber: number, Cell: number): cell {
+  return Board.modules[ModNumber].cells[Cell]
+}
+function QueryForPin(Board: board, ModNumber: number, Cell: number, Pin: number): pin {
+  return Board.modules[ModNumber].cells[Cell].pins[Pin]
+}
+function QueryForPinData(Board: board, ModNumber: number, Cell: number, Pin: number, key: string, value: number) { }
+
+
+function QueryFromBoardObject(Board: board, Module?: number, Cell?: Number, Pin?: number) {
+  if ((typeof (Module) === 'undefined') && ((typeof (Cell) != 'undefined') || (typeof (Pin) != 'undefined'))) {
+    throw new Error('To select a specific pin or cell a module is required')
+  } else if ((typeof (Cell) === 'undefined') && (typeof (Pin) != 'undefined')) {
     throw new Error('To select a specific pin a module and a cell are required')
   }
-  console.log('yep that worked');
-}
-function QueryFromBoardObject(Board: board, Module?: number, Cell?: Number, Pin?: number) {
 
 }
 
--- supportingJs/supporting.ts
+++ supportingJs/supporting.ts
@@ -1,12 +1,13 @@
 import * as fis from 'fs';
 import { Board, Cell, Module, Pin, board, cell, pin, module } from '../dtfDescription/dtfDescription.js';
+import * as path from 'path';
 
 function JSONSaver(FileName: string, json2Parse: any, extension: string) {
   let json = JSON.stringify(json2Parse);
   fis.writeFile(FileName + extension, json, function (err) { if (err) throw err; });
 }
 
-function emptyBoard(esn, time) {
+function emptyBoard(esn: string, time: number): board {
   let board = new Board(esn, time, emptyModules(emptyCells(emptyPins())));
   return board
 }
@@ -51,4 +52,53 @@ function dtfParse(path: string): board {
   return data;
 }
 
-export { JSONSaver, emptyBoard, arraysMatch, dtfParse }
+function findObjectFromPath(path: string, key: string, value: any): board | undefined {
+  if (dtfParse(path)[key] == value) {
+    return dtfParse(path);
+  }
+}
+
+function findFromESN(esn: string, Time?: number): board {
+  let finalBoard = emptyBoard(esn, 0);
+  let dtfFlies = fis.readdirSync("./").filter(file => file.includes('.dtf'));
+  if (typeof Time == 'undefined') {
+    dtfFlies.forEach(element => {
+      let testBoard = findObjectFromPath(element, "esn", esn);
+      if (typeof (testBoard) != 'undefined') {
+        if (testBoard.time > finalBoard.time) {
+          finalBoard = findObjectFromPath(element, "esn", esn);
+        }
+      }
+    });
+  }
+  else {
+    dtfFlies.forEach(element => {
+      let testBoard = findObjectFromPath(element, "esn", esn);
+      if (typeof (testBoard) != 'undefined') {
+        if (testBoard.time == Time) {
+          finalBoard = findObjectFromPath(element, "esn", esn);
+        }
+      }
+    });
+  }
+  return finalBoard;
+}
+
+function findLatestOf(Board: board, ModNumber: number): board | module | cell | pin {
+  let dtfFlies = fis.readdirSync("./").filter(file => file.includes('.dtf'));
+  let finalBoard = emptyBoard(Board.esn, Board.time)
+  dtfFlies.forEach(element => {
+    let testBoard = findObjectFromPath(element, "esn", Board.esn);
+    if (typeof (testBoard) != 'undefined') {
+      let testLength = Object.keys(testBoard.modules[ModNumber]).length;
+      if (testLength != 0) {
+        if (findObjectFromPath(element, "esn", Board.esn).time > finalBoard.time) {
+          finalBoard = findObjectFromPath(element, "esn", Board.esn);
+        }
+      }
+    }
+  });
+  return finalBoard.modules[ModNumber];
+}
+
+export { JSONSaver, emptyBoard, arraysMatch, dtfParse, findFromESN, findLatestOf, findObjectFromPath }
