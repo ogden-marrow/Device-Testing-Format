d5a66193b621d976c27a439e8d6ae38136a044ac
John Palenchar <jpalenchar.3@gmail.com>
2021-07-29 17:11:20 -0400

    day out 😁

--- src/testing.ts
+++ src/testing.ts
@@ -9,10 +9,24 @@ let add = {
   module: 0,
 }
 
-let pin = {
-  
+let dataFrame = {
+  StartTime: 24,
+  StopTime: 42,
+  RunTime: 52,
+  CycleRate: 35,
+  CycleCount: 245,
+  UpTiming: 345,
+  DownTiming: 346,
+  haltCycles: 567,
+  HightUp: 347,
+  HightDown: 23,
+  PKForce:85 ,
+  TipForce: 28,
+  Notes: ["test Note"],
+  Failures:["Something Bad"] ,
+  MSN: "255634"
 }
 
-let test = ChangeData(emptyBoard("nth45th",6969),add);
+let test = ChangeData(emptyBoard("nth45th",6969),add,dataFrame);
 
 // console.log(test.modules[0].cells[1].pins[0]);
