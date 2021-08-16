2a7225e0819671e8105726e0206ebae75ba54265
John Palenchar <jpalenchar.3@gmail.com>
2021-08-16 13:39:44 -0400

    updates to stuff

--- src/GetData.ts
+++ src/GetData.ts
@@ -1,34 +1,34 @@
 import { findFromESN, JSONSaver, findLatestOfModule, findLatestOfCell, findLatestOfPin } from './supporting';
 import { board, module, cell, pin } from './dtfDescription';
 
-function QueryFromESNTime(esn: string, Time?: number, Module?: number, Cell?: number, Pin?: number,): board | module | cell | pin {
+function QueryFromESNTime(esn: string, FPath: string, Time?: number, Module?: number, Cell?: number, Pin?: number,): board | module | cell | pin {
   if ((typeof (Module) == 'undefined') && ((typeof (Cell) != 'undefined') || (typeof (Pin) != 'undefined'))) {
     throw new Error('To select a specific pin or cell a module is required')
   } else if ((typeof (Cell) == 'undefined') && (typeof (Pin) != 'undefined')) {
     throw new Error('To select a specific pin a module and a cell are required')
   } else if (typeof (Module) == 'undefined') {
     if (typeof (Time) != 'undefined') {
-      return findFromESN(esn, Time)
+      return findFromESN(esn, FPath, Time)
     } else {
-      return findFromESN(esn)
+      return findFromESN(esn, FPath)
     }
   } else if (typeof (Module) == 'number' && (typeof (Cell) == 'undefined')) {
     if (typeof (Time) != 'undefined') {
-      return QueryForModule(findFromESN(esn, Time), Module);
+      return QueryForModule(findFromESN(esn, FPath, Time), Module);
     } else {
-      return QueryForModule(findFromESN(esn), Module);
+      return QueryForModule(findFromESN(esn, FPath), Module);
     }
   } else if (typeof (Cell) == 'number' && (typeof (Pin) == 'undefined')) {
     if (typeof (Time) != 'undefined') {
-      return QueryForCell(findFromESN(esn, Time), Module, Cell)
+      return QueryForCell(findFromESN(esn, FPath, Time), Module, Cell)
     } else {
-      return QueryForCell(findFromESN(esn), Module, Cell);
+      return QueryForCell(findFromESN(esn, FPath), Module, Cell);
     }
   } else if (typeof (Pin) == 'number') {
     if (typeof (Time) != 'undefined') {
-      return QueryForPin(findFromESN(esn, Time), Module, Cell, Pin);
+      return QueryForPin(findFromESN(esn, FPath, Time), Module, Cell, Pin);
     } else {
-      return QueryForPin(findFromESN(esn), Module, Cell, Pin);
+      return QueryForPin(findFromESN(esn, FPath), Module, Cell, Pin);
     }
   }
 }
